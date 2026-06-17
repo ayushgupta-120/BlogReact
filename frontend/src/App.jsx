1f8e6d0a8613d9299d46537a9c4eac1843bf284c
ayushgupta2study <ayushgupta2study@gmail.com>
2026-06-17 15:22:55 +0530

    modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const App = () => {
   return (
+    <>
     <div>AI Blogs</div>
+
+    <ol>
+      <li>AI HELTHCARE</li>
+      <li>AI SPORT</li>
+    </ol>
+    <form>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name ="name" /><br/>
+      <label htmlFor="email">email:</label>
+      <input type="text" id="email" name ="email" /><br/>
+      <label htmlFor="message">Message:</label>
+      <input type="text" id="message" name ="message" /><br/>
+      <button type="submit">Submit</button>
+    </form>
+    </>
   )
 }
 
